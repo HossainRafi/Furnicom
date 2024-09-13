cb40926f9a6d211a95ec7f9fb153c38bcba83ed8
HossainRafi <hossainrafi71@gmail.com>
2024-09-13 20:08:14 +0600

    create navMenu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,37 @@
+import { Link, NavLink } from "react-router-dom";
+
+const navItems = [
+  { path: "/", label: "Home" },
+  { path: "/shop", label: "Shop" },
+  { path: "/about", label: "About" },
+  { path: "/contact", label: "Contact" },
+];
+
+const NavMenu = () => {
+  return (
+    <ul>
+      {navItems.map((item, index) => (
+        <li key={index}>
+          <Link to={item.path}>{item.label}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 export const Navbar = () => {
   return (
     <header>
       <nav className="max-w-screen-2xl container mx-auto py-6 px-4 flex justify-between items-center">
         {/* nav logo */}
-        <a href="/">Logo</a>
+        <NavLink to="/" className="font-bold text-lg">
+          Panto
+        </NavLink>
 
         {/* desktop menu items */}
-        <div>Menu Items</div>
+        <div>
+          <NavMenu />
+        </div>
 
         {/* cart icon */}
         <div>cart</div>
