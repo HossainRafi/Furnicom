c21890df219fae262f279837252839571c5ab7e2
HossainRafi <hossainrafi71@gmail.com>
2024-09-13 15:23:17 +0600

    react router dom setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>
+      <Outlet />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "./index.css";
+import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children:[
+      {
+        path: "/",
+        element: <h1>Home</h1>
+      },
+      {
+        path: "/about",
+        element: <h1>About</h1>
+      },
+      {
+        path: "/contact",
+        element: <h1>Contact</h1>
+      },
+    ]
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
