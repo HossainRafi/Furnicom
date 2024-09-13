637361623a25d05c09b5cf13ae88945f113f7b3a
HossainRafi <hossainrafi71@gmail.com>
2024-09-13 20:55:25 +0600

    cart design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { FaCartPlus } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
 const navItems = [
@@ -32,16 +33,21 @@ export const Navbar = () => {
       <nav className="max-w-screen-2xl container mx-auto py-6 px-4 flex justify-between items-center">
         {/* nav logo */}
         <NavLink to="/">
-          <img src="../../public/logo.png" className="h-10 w-36" />
+          <img src="../../public/logo.png" className="h-8 w-32" />
         </NavLink>
 
         {/* desktop menu items */}
-        <div>
+        <div className="hidden md:flex">
           <NavMenu />
         </div>
 
         {/* cart icon */}
-        <div>cart</div>
+        <div className="hidden md:block relative cursor-pointer">
+          <FaCartPlus className="text-xl" />
+          <sup className="absolute -top-2 -right-1 bg-primary text-white rounded-full w-4 h-4 flex items-center justify-center text-xs">
+            0
+          </sup>
+        </div>
       </nav>
     </header>
   );
