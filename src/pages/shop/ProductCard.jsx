5d13e768e5d19878a9982bee02b2c7f3a0a08fda
HossainRafi <hossainrafi71@gmail.com>
2024-09-14 21:09:00 +0600

    Product shown in ui

--- src/pages/shop/ProductCard.jsx
+++ src/pages/shop/ProductCard.jsx
@@ -1,8 +1,26 @@
+import { getImgUrl } from "../../utils/getImageURL";
 
-
-
-export const ProductCard = ({products}) => {
+export const ProductCard = ({ product }) => {
   return (
-    <div>ProductCard</div>
-  )
-}
+    <div key={product.id} className="">
+      <div className="bg-[#FAFAFA]">
+        <img
+          src={getImgUrl(`${product.imageUrl}`)}
+          alt={product.name}
+          className="w-full h-full object-cover px-8 py-2 rounded"
+        />
+      </div>
+
+      <div className={`p-6 dark:bg-black bg-[#FAFAFA] shadow-sm`}>
+        <h4 className="text-base  mb-1">{product.category}</h4>
+        <h3 className="font-semibold text-xl mb-2">{product.name}</h3>
+        <p>{product.rating}</p>
+        <div className="mt-5 flex justify-between items-center">
+          <p className="text-secondary dark:text-white font-bold text-lg">
+            <sup>$</sup> <span className="">{product.price}</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/shop/Products.jsx
+++ src/pages/shop/Products.jsx
@@ -34,7 +34,7 @@ export const Products = ({ headline }) => {
         </div>
 
         {/* products card grid */}
-        <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
           {products.map((product, index) => (
             <ProductCard key={index} product={product} />
           ))}
