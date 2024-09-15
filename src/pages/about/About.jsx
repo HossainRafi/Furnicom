695b6f821aa7cd55145219c70531fa6820bb2b9b
HossainRafi <hossainrafi71@gmail.com>
2024-09-15 13:53:49 +0600

    about us done

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,19 @@
-
+import aboutBgImg from "../../assets/about-background.jpg";
+import { Experiences } from "../home/Experiences";
 
 export const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section>
+      {/* banner */}
+      <div
+        className="w-full h-[400px]  bg-no-repeat bg-cover  flex items-center justify-center text-white"
+        style={{ backgroundImage: `url(${aboutBgImg})` }}
+      >
+        <div className="">
+          <h1 className="text-5xl font-bold">About Us</h1>
+        </div>
+      </div>
+      <Experiences />
+    </section>
+  );
+};
