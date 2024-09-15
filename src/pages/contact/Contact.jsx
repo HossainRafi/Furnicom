151cdce58dbee4c281ee16f4d8efb7cc2ef3f095
HossainRafi <hossainrafi71@gmail.com>
2024-09-15 13:57:05 +0600

    contact done

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,19 @@
-
+import contactBgImg from "../../assets/contact-background.jpg";
+import { Materials } from "../home/Materials";
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section>
+      {/* banner */}
+      <div
+        className="w-full h-[400px]  bg-no-repeat bg-cover bg-center   flex items-center justify-center text-white"
+        style={{ backgroundImage: `url(${contactBgImg})` }}
+      >
+        <div className="">
+          <h1 className="text-5xl font-bold">Contact</h1>
+        </div>
+      </div>
+      <Materials />
+    </section>
+  );
+};
