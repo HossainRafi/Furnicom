643dd1517401a3502a9e1f35852d33495c1be2d7
HossainRafi <hossainrafi71@gmail.com>
2024-09-15 11:40:01 +0600

    load more functionality

--- src/pages/shop/ProductCard.jsx
+++ src/pages/shop/ProductCard.jsx
@@ -14,10 +14,9 @@ export const ProductCard = ({ product }) => {
       </div>
 
       <div className={`p-6 dark:bg-black bg-[#f6f6f6] shadow-sm rounded-b-xl`}>
-        <h4 className="text-base  mb-1">{product.category}</h4>
-        <h3 className="font-semibold text-xl mb-2">{product.name}</h3>
+        <h3 className="font-semibold text-xl mb-4">{product.name}</h3>
         <Rating rating={product.rating} />
-        <div className="mt-5 flex justify-between items-center">
+        <div className="mt-8 flex justify-between items-center">
           <p className="text-secondary dark:text-white font-bold text-lg">
             <sup>$</sup> <span className="">{product.price}</span>
           </p>
--- src/pages/shop/Products.jsx
+++ src/pages/shop/Products.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
 import { products } from "../../utils/products";
 import { ProductCard } from "./ProductCard";
+import btnIcon from "../../assets/button-icon.png";
 
 export const Products = ({ headline }) => {
-  const categories = ["Chair", "Beds", "Sofa", "Lamp"]; // categories
+  const categories = ["Chair", "Beds", "Sofa", "Lamp"]; // categories array
   const [selectedCategory, setSelectedCategory] = useState("Chair"); // state for selected category
+  const [visibleProducts, setVisibleProducts] = useState(4); // state for visible products
 
   // filter products based on category
   const filteredProducts = products.filter(
     (product) => product.category === selectedCategory
   );
 
+  // handling load more products
+  const loadMoreProducts = () => {
+    setVisibleProducts((prev) => prev + 4);
+  };
+
   return (
     <div>
       <div className="section-container">
-        {/* headlines */}
-        <h2 className="text-4xl font-bold text-center my-8">{headline}</h2>
+        {/* headline */}
+        <h2 className="text-4xl font-bold text-center my-8 ">{headline}</h2>
 
-        {/* category tab */}
+        {/* category option tabs */}
         <div className="bg-[#EEEEEE] max-w-md mx-auto rounded-md md:p-2 py-5 mb-16">
           <div className="flex flex-col sm:flex-row items-center md:justify-between justify-center gap-4">
             {categories.map((category) => (
@@ -25,6 +32,7 @@ export const Products = ({ headline }) => {
                 key={category}
                 onClick={() => {
                   setSelectedCategory(category);
+                  setVisibleProducts(4); // products shown per load
                 }}
                 className={`py-1 sm:px-5 px-8 rounded-md ${
                   selectedCategory === category
@@ -40,10 +48,23 @@ export const Products = ({ headline }) => {
 
         {/* products card grid */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-          {filteredProducts.map((product, index) => (
+          {filteredProducts.slice(0, visibleProducts).map((product, index) => (
             <ProductCard key={index} product={product} />
           ))}
         </div>
+
+        {/* load more button */}
+        {visibleProducts < filteredProducts.length && (
+          <div className="flex justify-center items-center mt-8">
+            <button
+              onClick={loadMoreProducts}
+              className="text-primary font-bold flex items-center px-4 py-2 rounded-full hover:text-white  gap-1 hover:bg-secondary transition-colors"
+            >
+              View More
+              <img src={btnIcon} />
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
