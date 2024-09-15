6c1b42c6e91012a8d3f4109a3119a96db10e8171
HossainRafi <hossainrafi71@gmail.com>
2024-09-15 18:56:45 +0600

    some design change for dark mode

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,13 @@ import { Hero } from "./Hero";
 import { Materials } from "./Materials";
 import { Testimonials } from "./Testimonials";
 
+
+import { BsMoonStars, BsSunFill } from "react-icons/bs";
+import { useContext } from "react";
+import { ThemeContext } from "./../../context/ThemeContext";
+
 export const Home = () => {
+  const { isDarkMode, toggleTheme } = useContext(ThemeContext);
   return (
     <>
       <Hero />
@@ -14,6 +20,20 @@ export const Home = () => {
       <Experiences />
       <Materials />
       <Testimonials />
+
+      {/* theme toggler button */}
+      <div className="bottom-10 md:bottom-28 right-6 md:right-20 z-40 fixed">
+        <button
+          onClick={toggleTheme}
+          className="focus:outline-none font-bold text-lg bg-black text-white p-3 rounded-full "
+        >
+          {isDarkMode ? (
+            <BsSunFill className="text-yellow-300 text-2xl" />
+          ) : (
+            <BsMoonStars className="text-2xl" />
+          )}
+        </button>
+      </div>
     </>
   );
 };
--- src/pages/shop/ProductCard.jsx
+++ src/pages/shop/ProductCard.jsx
@@ -5,7 +5,7 @@ import { getImgUrl } from "../../utils/getImageURL";
 export const ProductCard = ({ product }) => {
   return (
     <div key={product.id} className="">
-      <div className="bg-[#f6f6f6] rounded-t-xl">
+      <div className="dark:bg-[#EEEEEE] bg-[#f6f6f6] rounded-t-xl">
         <img
           src={getImgUrl(`${product.imageUrl}`)}
           alt={product.name}
@@ -13,12 +13,16 @@ export const ProductCard = ({ product }) => {
         />
       </div>
 
-      <div className={`p-6 dark:bg-black bg-[#f6f6f6] shadow-sm rounded-b-xl`}>
-        <h3 className="font-semibold text-xl mb-4">{product.name}</h3>
+      <div
+        className={`p-6 dark:bg-[#EEEEEE] bg-[#f6f6f6] shadow-sm rounded-b-xl`}
+      >
+        <h3 className="font-semibold text-xl mb-4 dark:text-black">
+          {product.name}
+        </h3>
         <Rating rating={product.rating} />
         <div className="mt-8 flex justify-between items-center">
-          <p className="text-secondary dark:text-white font-bold text-lg">
-            <sup>$</sup> <span className="">{product.price}</span>
+          <p className="text-secondary dark:text-black font-bold text-lg">
+            $ <span className="">{product.price}</span>
           </p>
           <button className="bg-secondary p-2 rounded-full text-white">
             <FaCartPlus />
