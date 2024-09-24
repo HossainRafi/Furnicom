df8e77c206f76bdc0ee76dcd7c8acc511a25fca9
HossainRafi <hossainrafi71@gmail.com>
2024-09-24 22:29:14 +0600

    theme problem for home solved

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,12 +5,7 @@ import { Hero } from "./Hero";
 import { Service } from "./Service";
 import { Testimonials } from "./Testimonials";
 
-import { BsMoonStars, BsSunFill } from "react-icons/bs";
-import { useContext } from "react";
-import { ThemeContext } from "./../../context/ThemeContext";
-
 export const Home = () => {
-  const { isDarkMode, toggleTheme } = useContext(ThemeContext);
   return (
     <>
       <Hero />
@@ -19,20 +14,6 @@ export const Home = () => {
       <Experiences />
       <Service />
       <Testimonials />
-
-      {/* theme toggler button */}
-      <div className="bottom-10 md:bottom-28 right-6 md:right-20 z-40 fixed">
-        <button
-          onClick={toggleTheme}
-          className="focus:outline-none font-bold text-lg bg-black text-white p-3 rounded-full "
-        >
-          {isDarkMode ? (
-            <BsSunFill className="text-yellow-300 text-2xl" />
-          ) : (
-            <BsMoonStars className="text-2xl" />
-          )}
-        </button>
-      </div>
     </>
   );
 };
